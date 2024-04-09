a44d224a9f2c05a327d904edc075bf2d72c7f866
devhaute <devhaute@gmail.com>
2024-04-09 23:17:42 +0900

    초기 환경 세팅

---
+++ VisionScanner/VisionScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionScannerApp.swift
+//  VisionScanner
+//
+//  Created by kai on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
